ebe87a7d0bd2d73fb93f707815fc699d9c4d09ea
Alistair Driscoll <pallythecoder@gmail.com>
2025-10-31 20:31:41 +0000

    amend settings to serve static correctly

--- recipe_reapers/settings.py
+++ recipe_reapers/settings.py
@@ -132,16 +132,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# --- Static files ---
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = "media/"
+if os.environ.get("DEBUG") == "True":
+    STATICFILES_STORAGE = "django.contrib.staticfiles."
+    "storage.StaticFilesStorage"
+else:
+    STATICFILES_STORAGE = "whitenoise.storage."
+    "CompressedManifestStaticFilesStorage"
+
+# --- Media (Cloudinary) ---
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
-CLOUDINARY_URL = os.environ["CLOUDINARY_URL"]
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
