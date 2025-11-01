f202a2fbcc6621f37bf3a8380ab1b469e6593e6f
Alistair Driscoll <pallythecoder@gmail.com>
2025-11-01 15:06:56 +0000

    tweak to recipe model

--- recipe_search/models.py
+++ recipe_search/models.py
@@ -11,3 +11,6 @@ class Recipe(models.Model):
     title = models.CharField(max_length=100, blank=False, null=False)
     image = CloudinaryField(null=True, blank=True)
     recipe = models.TextField()
+
+    def __str__(self):
+        return self.title
