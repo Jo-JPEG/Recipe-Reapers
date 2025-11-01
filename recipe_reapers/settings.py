9c17022b97266308aab48800c5fc08bc6952dce8
Alistair Driscoll <pallythecoder@gmail.com>
2025-11-01 11:53:50 +0000

    Add postgresql database and amend settings accordingly

--- recipe_reapers/settings.py
+++ recipe_reapers/settings.py
@@ -15,6 +15,7 @@ import os
 
 if os.path.isfile("env.py"):
     import env  # noqa
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,12 +87,15 @@ CSRF_TRUSTED_ORIGINS = ["https://*.codeanyapp.com", "https://*.herokuapp.com"]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if "DATABASE_URL" in os.environ:
+    DATABASES = {"default": dj_database_url.parse(os.environ["DATABASE_URL"])}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
 
 
 # Password validation
