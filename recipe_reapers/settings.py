a2e6baa959c9f98c557fdc6ffb6f538db8084200
curtisnlogan <curtisnlogan@gmail.com>
2025-11-01 17:29:31 +0000

    chore: update comments in settings.py for clarity and reordered installed_apps for best practice

--- recipe_reapers/settings.py
+++ recipe_reapers/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
+# manage DEBUG at environment level to avoid potential security leaks
 DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = [
@@ -46,9 +46,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "recipe_search",
     "cloudinary",
     "cloudinary_storage",
+    "recipe_search",
 ]
 
 MIDDLEWARE = [
