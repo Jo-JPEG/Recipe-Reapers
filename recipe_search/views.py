3b6e585876c1954f5cefcc40afad84ccb1c80168
Alistair Driscoll <pallythecoder@gmail.com>
2025-11-02 19:55:48 +0000

    Implement search functionality for backend

--- recipe_search/views.py
+++ recipe_search/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.db.models import Q
+
+from .models import Recipe
 
 
 def index(request):
@@ -6,4 +9,22 @@ def index(request):
     View to return to the index page
     """
 
-    return render(request, "recipe_search/index.html")
+    if request.method == "GET":
+        return render(request, "recipe_search/index.html")
+
+    else:
+        query = request.GET.get("query", "")
+
+        relevant_recipes = Recipe.objects.filter(
+            Q(title__icontains=query) | Q(recipe__icontains=query)
+        )
+
+        context = {
+            'relevent_recipes': relevant_recipes,
+        }
+
+        return render(
+            request,
+            "recipe_search/index.html",
+            context,
+        )
